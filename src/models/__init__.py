a7fd950fdc13e4c8ccd73cedfe0db5821d8158ec
anskydy-tz <anskydy@gmail.com>
2026-01-20 14:59:04 +0300

    Update __init__.py под таблицу рецептов

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,11 +1,22 @@
 """
-Пакет моделей машинного обучения для предсказания рецептов терразитовой штукатурки
+Пакет моделей машинного обучения для Terrazite AI
 """
 
-from .terrazite_model import TerraziteRecipeModel
+from .terrazite_model import (
+    TerraziteModel,
+    MultiTaskLoss,
+    TerraziteEnsemble,
+    create_model
+)
 from .simple_classifier import SimpleAggregateClassifier
+from .trainer import ModelTrainer, create_trainer
 
 __all__ = [
-    'TerraziteRecipeModel',
-    'SimpleAggregateClassifier'
+    'TerraziteModel',
+    'MultiTaskLoss',
+    'TerraziteEnsemble',
+    'create_model',
+    'SimpleAggregateClassifier',
+    'ModelTrainer',
+    'create_trainer'
 ]
