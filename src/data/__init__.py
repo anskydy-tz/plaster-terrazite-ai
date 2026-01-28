390dce2f9ff1d7f5badfad48f6bfba82ff384d1f
anskydy-tz <anskydy@gmail.com>
2026-01-28 17:38:14 +0300

    Update __init__.py отладка

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -2,15 +2,21 @@
 Модуль для обработки данных для терразитовой штукатурки
 """
 
-from .loader import DataLoader, RecipeLoader
+# Импорт классов из модулей пакета data
+from .loader import DataLoader, RecipeLoader, TerraziteDataset, RecipeData
 from .processor import DataProcessor
 from .visualizer import DataVisualizer
 from .augmentor import DataAugmentor
+from .component_analyzer import ComponentAnalyzer
 
+# Определение публичного API пакета
 __all__ = [
     'DataLoader',
-    'RecipeLoader', 
+    'RecipeLoader',
+    'TerraziteDataset',
+    'RecipeData',
     'DataProcessor',
-    'DataVisualizer',
-    'DataAugmentor'
+    'DataVisualizer', 
+    'DataAugmentor',
+    'ComponentAnalyzer'
 ]
